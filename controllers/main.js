0238ab197e9f5b0a9bde6602be4ab1928f517957
uriel197 <60386860+uriel197@users.noreply.github.com>
2024-01-07 13:03:32 +0200

    Added authentication token

--- controllers/main.js
+++ controllers/main.js
@@ -1,11 +1,18 @@
 const CustomAPIError = require("../errors/custom-error");
+const jwt = require('jsonwebtoken');
 
 const login = async (req, res) => {
     const { username, password } = req.body;
     if(!username || !password) {
         throw new CustomAPIError('Please provide user name and passwrd', 400);
     }
-    res.send(`Welcome Back ${username}`);
+
+    // just for Demo, normally provided by DB!!
+    const id = new Date().getDate();
+
+    // keep payload small, and ommit sensitive info like passwrd!
+    const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'})
+    res.status(200).json({ msg: 'user created', token });
 }
 
 const dashboard = async (req, res) => {
