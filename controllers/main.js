500eee9e6e59c2208b60bb84b45502ce4f51c356
uriel197 <60386860+uriel197@users.noreply.github.com>
2024-01-09 12:41:20 +0200

    Added Authentication token and refactored error functions

--- controllers/main.js
+++ controllers/main.js
@@ -1,23 +1,28 @@
-const CustomAPIError = require("../errors/custom-error");
+const { BadRequestError } = require("../errors");
 const jwt = require('jsonwebtoken');
 
 const login = async (req, res) => {
     const { username, password } = req.body;
     if(!username || !password) {
-        throw new CustomAPIError('Please provide user name and passwrd', 400);
+        throw new BadRequestError('Please provide user name and passwrd');
     }
 
     // just for Demo, normally provided by DB!!
     const id = new Date().getDate();
 
     // keep payload small, and ommit sensitive info like passwrd!
-    const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'})
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {expiresIn: '30d'})
     res.status(200).json({ msg: 'user created', token });
 }
 
 const dashboard = async (req, res) => {
     const luckyNumber = Math.floor(Math.random() * 100);
-    res.status(200).json({ msg: `Hello Boss`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}`});
+
+    res.status(200).json({ 
+        msg: `Hello ${req.user.username}`, 
+        secret: `Here is your authorized data, your lucky number is ${luckyNumber}`
+    });
+
 }
 
 module.exports = {
--- public/browser-app.js
+++ public/browser-app.js
@@ -7,38 +7,38 @@ const btnDOM = document.querySelector('#data')
 const tokenDOM = document.querySelector('.token')
 
 formDOM.addEventListener('submit', async (e) => {
-  formAlertDOM.classList.remove('text-success')
-  tokenDOM.classList.remove('text-success')
-
-  e.preventDefault()
-  const username = usernameInputDOM.value
-  const password = passwordInputDOM.value
-
-  try {
-    const { data } = await axios.post('/api/v1/login', { username, password })
-
-    formAlertDOM.style.display = 'block'
-    formAlertDOM.textContent = data.msg
-
-    formAlertDOM.classList.add('text-success')
-    usernameInputDOM.value = ''
-    passwordInputDOM.value = ''
-
-    localStorage.setItem('token', data.token)
-    resultDOM.innerHTML = ''
-    tokenDOM.textContent = 'token present'
-    tokenDOM.classList.add('text-success')
-  } catch (error) {
-    formAlertDOM.style.display = 'block'
-    formAlertDOM.textContent = error.response.data.msg
-    localStorage.removeItem('token')
-    resultDOM.innerHTML = ''
-    tokenDOM.textContent = 'no token present'
+    formAlertDOM.classList.remove('text-success')
     tokenDOM.classList.remove('text-success')
-  }
-  setTimeout(() => {
-    formAlertDOM.style.display = 'none'
-  }, 2000)
+
+    e.preventDefault()
+    const username = usernameInputDOM.value
+    const password = passwordInputDOM.value
+
+    try {
+        const { data } = await axios.post('/api/v1/login', { username, password })
+
+        formAlertDOM.style.display = 'block'
+        formAlertDOM.textContent = data.msg
+
+        formAlertDOM.classList.add('text-success')
+        usernameInputDOM.value = ''
+        passwordInputDOM.value = ''
+
+        localStorage.setItem('token', data.token)
+        resultDOM.innerHTML = ''
+        tokenDOM.textContent = 'token present'
+        tokenDOM.classList.add('text-success')
+    } catch (error) {
+        formAlertDOM.style.display = 'block'
+        formAlertDOM.textContent = error.response.data.msg
+        localStorage.removeItem('token')
+        resultDOM.innerHTML = ''
+        tokenDOM.textContent = 'no token present'
+        tokenDOM.classList.remove('text-success')
+    }
+    setTimeout(() => {
+        formAlertDOM.style.display = 'none'
+    }, 2000)
 })
 
 btnDOM.addEventListener('click', async () => {
