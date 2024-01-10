3f7e40173797950d25d3602049a3334098f05639
uriel197 <60386860+uriel197@users.noreply.github.com>
2024-01-10 13:03:38 +0200

    comment that explains extends Error

--- errors/custom-error.js
+++ errors/custom-error.js
@@ -5,3 +5,16 @@ class CustomAPIError extends Error {
   }
   
   module.exports = CustomAPIError;
+
+    /******** Comments *********
+
+*** 1: In JavaScript, Error is a built-in object that represents an error that occurs during the execution of a program. It is used to signal that an exceptional condition has occurred, causing the normal flow of the program to be disrupted. The Error object can be extended to create custom error types, as shown in our code.
+properties and methods provided by the Error object:
+
+message: A human-readable description of the error.
+name: A string representing the name of the error (e.g., "Error", "SyntaxError", "TypeError").
+stack: A stack trace that indicates the sequence of function calls that led to the error.
+
+In our code, the CustomAPIError class extends the built-in Error class. It doesn't add any new properties or methods, but it allows you to create instances of errors with a specific name (CustomAPIError) for better identification and handling in our code. We can use this custom error class to throw and catch errors in a way that is more meaningful to our application.
+
+   */
